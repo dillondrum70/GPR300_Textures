95fac7667fd431243562db1bedfff2856edf565c
Dillon Drummond <dillon.drummond@mymail.champlain.edu>
2023-03-06 16:24:26 -0500

    Fixed seams

--- GPR300_Textures/GPR300_Textures/EW/ShapeGen.cpp
+++ GPR300_Textures/GPR300_Textures/EW/ShapeGen.cpp
@@ -155,23 +155,11 @@ namespace ew {
 				float y = cosf(phi);
 				float z = sinf(phi) * cosf(theta);
 
-				float uvX= sinf(phi) * sinf(theta + glm::three_over_two_pi<float>());
-				float uvZ = sinf(phi) * cosf(theta + glm::three_over_two_pi<float>());
-
-				if (j == 0)
-				{
-					uvX = 0;
-				}
-				else if (j == numSegments)
-				{
-					uvX = 1;
-				}
-
 				glm::vec3 position = radius * glm::vec3(x, y, z);
 				glm::vec3 normal = glm::normalize(glm::vec3(x, y, z));
 				glm::vec2 uv = glm::vec2(
-					((atan2(uvZ, uvX) / glm::two_pi<float>()) + 1) / 2,
-					acosf(y) / glm::pi<float>());
+					j / (float)numSegments,
+					-i / (float)numSegments);
 				//printf("%f, %f\n", uv.x, uv.y);
 				meshData.vertices.push_back({ position, normal, uv });
 			}
@@ -264,35 +252,15 @@ namespace ew {
 		{
 			glm::vec3 pos = meshData.vertices[i + 1].position;
 			glm::vec3 normal = glm::normalize((pos - meshData.vertices[0].position));
-			glm::vec2 uv = glm::vec2(((atan2(sin(i * thetaStep + glm::pi<float>()), cos(i * thetaStep + glm::pi<float>())) / (glm::two_pi<float>())) + .5), 1);
-			printf("%f\n", uv.x);
-
-			if (i == 0)
-			{
-				uv.x = 0;
-			}
-			else if (i == numSegments)
-			{
-				uv.x = 1;
-			}
+			glm::vec2 uv = glm::vec2(i / (float)numSegments, 1);
 			meshData.vertices.push_back(Vertex(pos, normal, uv));
 		}
-		printf("\n\n");
 		//Side bottom ring
 		for (int i = 0; i <= numSegments; i++)
 		{
 			glm::vec3 pos = meshData.vertices[bottomCenterIndex + i + 1].position;
 			glm::vec3 normal = glm::normalize((pos - meshData.vertices[bottomCenterIndex].position));
-			glm::vec2 uv = glm::vec2(((atan2(sin(i * thetaStep + glm::pi<float>()), cos(i * thetaStep + glm::pi<float>())) / (glm::two_pi<float>())) + .5), 0);
-			if (i == 0)
-			{
-				uv.x = 0;
-			}
-			else if (i == numSegments)
-			{
-				uv.x = 1;
-			}
-			printf("%f\n", uv.x);
+			glm::vec2 uv = glm::vec2(i / (float)numSegments, 0);
 			meshData.vertices.push_back(Vertex(pos, normal, uv));
 		}
 
